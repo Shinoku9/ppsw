a88830f13929c198f66dafe2ec30caf27c851aba
Tomasz <tomekb14@poczta.fm>
2019-06-06 13:38:40 +0200

    modified timery

--- Timery/timer.c
+++ Timery/timer.c
@@ -11,23 +11,33 @@
 
 
 void InitTimer0(void){
-	T0TCR = T0TCR |	COUNTER_ENABLE_bm;
+	T0TCR = T0TCR |	COUNTER_ENABLE_bm; //wlaczenie timera
 }
 
 void WaitOnTimer0(unsigned int uiTime){
-	T0TCR = T0TCR | COUNTER_RESET_bm;
-	T0TCR = T0TCR & ~COUNTER_RESET_bm;	
+	T0TCR = T0TCR | COUNTER_RESET_bm; //zerowanie timera
+	T0TCR = T0TCR & ~COUNTER_RESET_bm;	//zakonczenie resetowania timera
 	
-	while(T0TC < (uiTime*FREQ_PCLK)){}
+	while(T0TC < (uiTime*FREQ_PCLK)){} //petla opozniajaca o wartosc podana w argumencie
 }
 
 void InitTimer0Match0(unsigned int iDelayTime){
-	T0MR0 = (iDelayTime*FREQ_PCLK);
-	T0MCR = T0MCR | MR0_INTERRUPT_bm | RESET_ON_MR0_bm;
-	InitTimer0();
+	T0MR0 = (iDelayTime*FREQ_PCLK); //obliczenie ilosci cykli i wpisanie wartosci do rejestru porownujacego
+	T0MCR = T0MCR | (MR0_INTERRUPT_bm | RESET_ON_MR0_bm); //ustawienie bitow odpowiadajacych za resetowanie timera po osiagnieciu wartosci i ustawienie flagi przerwania
+	InitTimer0(); //wlaczenie timera
 }
 
 void WaitOnTimer0Match0(){
-	while(T0IR!=(T0IR | MR0_INTERRUPT_bm)){}
-	T0IR |= INTERRUPT_ON_MR0_bm;
+	while((T0IR & MR0_INTERRUPT_bm)==0){} //petla opozniajaca do momentu zapalenia flagi przerwania
+	T0IR = INTERRUPT_ON_MR0_bm; //resetowanie przerwania
 }
+
+
+
+//co powoduje reset timera oraz ustawienie flagi przerwania
+
+
+//RESET - ustawienie bitu RESET_ON_MR0 w rejestrze MCR (liczenie impulsów od ppczatku do momentu wyrownania z wartoscia wpisana w T0MR0)
+
+
+//FLAGA - ustawienie bitu MR0_INTERRUPT w rejestrze MCR
